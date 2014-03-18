27e5a499ea150964ef8185f9b7e3c9197bc475cb
three18ti <three18ti@gmail.com>
2014-03-18 13:12:45 -0600

    added minimal recipe

---
+++ recipes/minimal.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: linux-base
+# Recipe:: minimal
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'set_hostname'
+include_recipe 'chef-client'
+
+ssh_users = []
+search(:users, "*:*").each do |user| 
+    next unless /-/.match(user["id"]) && user['ssh_keys']
+
+    username, hostname = user['id'].split(/-/,2)
+    next if node['hostname'] == hostname && username != 'root'
+
+    ssh_users << user["id"]    
+end
+
+node.normal[:ssh_keys] = { 'root' => ssh_users }
+
+include_recipe 'ssh-keys'
+
+
+#log "ssh keys" do
+#    message "ssh_users: #{ssh_users}"
+#    level :warn
+#end
+
+#node.normal[:ssh_keys] = { 'root' => ["root-chef-workstation"] }
